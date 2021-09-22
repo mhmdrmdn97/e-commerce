558d4b4645fbfcaeec5f72d337ba5657cb71ffe1
mhmdrmdn <cash_cena@hotmail.com>
2021-09-23 01:44:07 +0300

    putting items in dom

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ for(elem in data){
 }
 
 console.log(items);
+const itemsId= document.getElementById('items');
+const imgy = [];
+const namey = [];
+const pricey = [];
+for(elem in data){
+    const containerCreate = document.createElement('div');
+const container = itemsId.appendChild(containerCreate);
+container.className = 'item';
 
+    const imgName = document.createElement('div');
+    imgName.style.cssText = `background: url(${items[elem].img}); background-position: center;background-repeat: no-repeat;background-size: cover; height: 10rem;`
+ imgy[elem] =container.appendChild(imgName);
+    const divName = document.createElement('div');
+    divName.style.cssText = `padding-left:40%; font-size:2rem; padding-bottom:3%;`;
+    namey[elem] =container.appendChild(divName);
+namey[elem].innerHTML= items[elem].name;
+    const divPrice = document.createElement('div');
+    divPrice.style.cssText = `padding-left:40%; font-size:2rem;padding-bottom:2%;`;
+ pricey[elem] =container.appendChild(divPrice);
+pricey[elem].innerHTML= `${items[elem].price}$`;
+}
 }
 overallFunction()
